ed491e1e518c957b31ce0c66a016425d7faf3985
BEN <112600905+ALiENxBEN@users.noreply.github.com>
2024-06-29 00:32:43 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -186,12 +186,15 @@ async def fetch_user_tds(user_id, force=False):
 
 def progress_bar(pct):
     if isinstance(pct, str):
-        pct = float(pct.strip('%'))
+        pct = float(str(pct).strip('%'))
     p = min(max(pct, 0), 100)
-    cFull = int((p + 5)// 10)
-    p_str = '●' * cFull
-    p_str += '○' * (10 - cFull)
-    return p_str
+    cFull = int(p // 8)
+    cPart = int(p % 8 - 1)
+    p_str = '■' * cFull
+    if cPart >= 0:
+        p_str += ['▤', '▥', '▦', '▧', '▨', '▩', '■'][cPart]
+    p_str += '□' * (12 - cFull)
+    return f"[{p_str}]"
 
 
 def source(self):
