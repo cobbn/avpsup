cf10780e4e66a64fe8a1716faca8aa82a7b9419c
BEN <112600905+ALiENxBEN@users.noreply.github.com>
2024-06-29 01:33:56 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -241,7 +241,7 @@ def get_readable_message():
         else:
             msg += f"<blockquote>Size: {download.size()}"
         msg += f"\n<b>➪ᴇʟᴀᴘꜱᴇᴅ</b>: {get_readable_time(time() - download.message.date.timestamp())}</blockquote>"
-        msg += f"\n<blockquote>/stop_{download.gid()[:8]}</blockquote>\n\n"
+        msg += f"\n<b>➪🔴</b>:/stop_{download.gid()[:8]}</blockquote>\n\n"
     if len(msg) == 0:
         return None, None
     if tasks > STATUS_LIMIT:
