963bb51e20e8b3a6a73c301bf2833d564a36074a
reaperbots <168705970+cobbn@users.noreply.github.com>
2024-07-22 14:01:42 +0530

    fixed

--- bot/__init__.py
+++ bot/__init__.py
@@ -395,7 +395,7 @@ if ospath.exists('shorteners.txt'):
 PORT = environ.get('PORT','8080')
 Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT} --worker-class gevent", shell=True)
 
-srun(["xnox", "-d", "--profile=."])
+srun(["qbittorrent-nox", "-d", "--profile=."])
 if not ospath.exists('.netrc'):
     with open('.netrc', 'w'):
        pass
@@ -407,7 +407,7 @@ with open("a2c.conf", "a+") as a:
     if TORRENT_TIMEOUT is not None:
         a.write(f"bt-stop-timeout={TORRENT_TIMEOUT}\n")
     a.write(f"bt-tracker=[{trackers}]")
-srun(["xria", "--conf-path=/usr/src/app/a2c.conf"])
+srun(["aria2c", "--conf-path=/usr/src/app/a2c.conf"])
 
 if ospath.exists('accounts.zip'):
     if ospath.exists('accounts'):
